d947ac654ee9e9f00333325ca937e4fdd3c84f81
vivian <tongvivian0326@gmail.com>
2023-10-11 14:40:45 +0800

    buddies screen initial

---
+++ trip-mingle/src/screen/EnquireScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import BuddiesPageStyleSheet from "../StyleSheet/BuddiesPageCss";
+
+export default function EnquireScreen() {
+  return (
+    <View style={BuddiesPageStyleSheet.container}>
+      <Text>Enquire!</Text>
+    </View>
+  );
+}
