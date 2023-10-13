409ef1a57635fa431ff1e287c31f82dfc192dd6b
Henry <yhnganaa@gmail.com>
2023-10-13 20:18:34 +0800

    added JWT service and AuthGuard in nest-server, testing module provided

---
+++ nestjs/src/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from 'src/jwt/jwt.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    if (!req.headers.authorization) return false;
+
+    const token = req.headers?.authorization.split(' ')[1];
+
+    if (!token) return false;
+    const payload = this.jwtService.decode(token);
+
+    if (payload.id && typeof payload.id === 'number') {
+      req.payload = payload;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
